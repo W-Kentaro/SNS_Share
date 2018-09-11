4bf262fa828a50f3a70407549dfe7747e62544e6
W-Kentaro <watanabe@kirksville.co.jp>
2018-09-11 19:59:12 +0900

    samplepage add

--- src/samplePage.js
+++ src/samplePage.js
@@ -1,3 +1,18 @@
 'use strict';
 
-var ShareText = new ShareTextEmbedded(share);
+
+var ShareText01 = new ShareTextEmbedded(share);
+var ShareText02 = new ShareTextEmbedded({
+  url: 'https://github.com/W-Kentaro/ShareTextEmbedded',
+  text: 'ShareTextEmbeddedはシェア文を自動でエンコードしてリンクを埋め込む関数です。',
+  twitter: {
+    elem: '.js-share-twitter',
+    hash: 'ShareTextEmbedded',
+  },
+  facebook: {
+    elem: '.js-share-facebook',
+  },
+  line: {
+    elem: '.js-share-line',
+  }
+});
